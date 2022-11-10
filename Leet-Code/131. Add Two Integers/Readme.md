b9a35490832315d7e47676037c63b3e129e475d1
AlaminPu1007 <alamin66.sit@gmail.com>
2022-11-10 20:48:31 +0600

    add two integers problem solved

---
+++ Leet-Code/131. Add Two Integers/Readme.md
@@ -0,0 +1,12 @@
+## Add Two Integers
+
+[Open problem](https://leetcode.com/problems/add-two-integers/)
+
+**Approach :**<br>
+
+- `return num1 + num2`
+
+**Complexity :**<br>
+
+- Time-Complexity: `O(1)`
+- Space-Complexity: `O(1)`
---
+++ Leet-Code/131. Add Two Integers/add_two_integers.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int sum(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+};
+
+int main(void)
+{
+    Solution obj;
+    obj.sum(5, 7);
+    return 0;
+}
