bd41f2aa669d8cd9feb806b8c37ac1654e53b5cf
alaminpu1007 <alamin66.sit@gmail.com>
2025-07-02 20:13:44 +0600

    865_Smallest_Subtree_with_all_the_Deepest_Nodes, this problem solution has been added

---
+++ Leet-Code/869. Smallest Subtree with all the Deepest Nodes/865_Smallest_Subtree_with_all_the_Deepest_Nodes.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class TreeNode
+{
+public:
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode(int _v)
+    {
+        left = NULL;
+        right = NULL;
+        val = _v;
+    }
+};
+
+class Solution
+{
+public:
+    TreeNode *subtreeWithAllDeepest(TreeNode *root)
+    {
+        return DFS(root).first;
+    }
+
+private:
+    pair<TreeNode *, int> DFS(TreeNode *root)
+    {
+        if (!root)
+            return {NULL, 1};
+
+        auto left = DFS(root->left);
+        auto right = DFS(root->right);
+
+        if (left.second > right.second)
+            return {left.first, left.second + 1};
+
+        if (left.second < right.second)
+            return {right.first, right.second + 1};
+
+        return {root, left.second + 1};
+    }
+};
+
+int main(void)
+{
+    Solution obj;
+
+    return 0;
+}
